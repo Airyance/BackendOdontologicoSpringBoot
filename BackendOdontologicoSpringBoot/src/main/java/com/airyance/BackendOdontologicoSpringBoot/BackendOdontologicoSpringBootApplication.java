d1b84e1116f5bfeb98e784be97fd323ed3f85210
airyance <airyance.mazo@gmail.com>
2022-04-14 00:12:50 -0500

    version incial

---
+++ BackendOdontologicoSpringBoot/src/main/java/com/airyance/BackendOdontologicoSpringBoot/BackendOdontologicoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.airyance.BackendOdontologicoSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendOdontologicoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendOdontologicoSpringBootApplication.class, args);
+	}
+
+}
